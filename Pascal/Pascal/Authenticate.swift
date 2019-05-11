0738a9a5b91e48d1b0f095af2a790598d04ae459
Ravi Patel <rdp2606@gmail.com>
2019-05-11 13:37:35 -0700

    user authentication

---
+++ Pascal/Pascal/Authenticate.swift
@@ -0,0 +1,122 @@
+//
+//  Authenticate.swift
+//  Pascal
+//
+//  Created by Ravi Patel on 5/11/19.
+//  Copyright © 2019 ECE 140. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import GoogleSignIn
+import FacebookCore
+import FacebookLogin
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// global DispatchGroup for thread control
+let global_dispatchGroup = DispatchGroup()
+func run(completion: @escaping() -> Void)
+{
+    DispatchQueue.main.async{
+        completion()
+    }
+}
+
+
+class Authenticate{
+    static let instance = Authenticate()
+    
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // For Google sign in
+    var google_sign_in_success = false
+    var global_google_uiDeletage:GIDSignInUIDelegate!
+    func authenticate_google_signIn(success: @escaping () -> ())
+    {
+        global_dispatchGroup.enter()
+        run{
+            print("before google sign in...")
+            GIDSignIn.sharedInstance()?.uiDelegate = self.global_google_uiDeletage;
+            GIDSignIn.sharedInstance()?.signIn();
+            success()
+        }
+    }
+    
+    
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // For user registration
+    var registration_isValid:Bool = false
+    func register_user(email: String, passWord: String, success: @escaping () -> (), failure: @escaping () -> ())
+    {
+        Auth.auth().createUser(withEmail: email, password: passWord) { (user, error) in
+            // error creating new user
+            if let _ = error{
+                failure()
+            }
+            else{
+                if let _ = Auth.auth().currentUser{
+                    Auth.auth().currentUser?.sendEmailVerification(completion: { (error) in
+                        // cound not send email verification
+                        if let _ = error{
+                            failure()
+                        }
+                        else{
+                            success()
+                        }
+                    })
+                }
+                else{
+                    // user alread in the database, cannot recreate user
+                    failure()
+                }
+            }
+        }
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // For user authentication
+    func authenticate_user_login(email: String, passWord: String, success: @escaping () -> (), failure: @escaping () -> ())
+    {
+        Auth.auth().signIn(withEmail: email, password: passWord) { (user, error) in
+            if let _ = error{
+                failure()
+            }
+            else{
+                print("auth success")
+                // check if user has verified their email
+                if let user = Auth.auth().currentUser{
+                    if !user.isEmailVerified{
+                        failure()
+                    }
+                    else {
+                        success()
+                    }
+                }
+
+            }
+        }
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // For sending password reset link
+    func send_password_reset_link(email: String, success: @escaping () -> (), failure: @escaping () -> ()){
+        
+        Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+            // error sending password reset link
+            if error != nil{
+                failure()
+            }
+            else{
+                // password reset link send
+                success()
+            }
+        }
+    }
+}
--- Pascal/Pascal/ViewController.swift
+++ Pascal/Pascal/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // For google sign in
+        Authenticate.instance.global_google_uiDeletage = self
+        
+//        Authenticate.instance.register_user(email: "enmurill@ucsd.edu", passWord: "rick_g", success: {
+//            print("user registration successful")
+//        }) {
+//            print("failed to register user")
+//        }
     }
 
 
+    @IBAction func google_sign_in(_ sender: Any) {
+
+        Authenticate.instance.authenticate_google_signIn {
+            if Authenticate.instance.google_sign_in_success {
+                // successfull sign in via google account
+                print("successfull sign in via google account")
+            }
+            else{
+                // unsuccessfull sign in via google account
+                print("unsuccessfull sign in via google account")
+            }
+        }
+    }
+    
+    
 }
 
